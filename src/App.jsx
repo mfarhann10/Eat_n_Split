e58e97b3ae7d6f7c9b28adb94b279f74aaeaf250
mfarhann10 <mfarhann1009@gmail.com>
2024-11-22 14:59:21 +0700

    add Close an add friend button to show Comp FormAddFriend

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
+import { useState } from "react";
 import Button from "./Components/Button";
 import FormAddFriend from "./Components/FormAddFriend";
 import FormSplitBill from "./Components/FormSplitBill";
 import FriendList from "./Components/Friendlist";
 
 function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+
+  function handleShowAdd() {
+    setShowAddFriend((show) => !show);
+  }
+
   return (
     <div className="app grid grid-cols-[34rem_44rem] gap-16 min-h-[66vh] items-start mt-5">
       <div className="sidebar rounded-lg shadow-lg p-6 space-y-6">
         <FriendList />
-        <FormAddFriend />
-        <div className="flex flex-row-reverse">
-          <Button>Add Friend</Button>
-        </div>
+        {showAddFriend && <FormAddFriend />}
+        {
+          <div className="flex flex-row-reverse">
+            <Button onClick={handleShowAdd}>
+              {showAddFriend ? "Close" : "Add Friend"}
+            </Button>
+          </div>
+        }
       </div>
       <FormSplitBill />
     </div>
--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,7 +1,10 @@
 /* eslint-disable react/prop-types */
-function Button({ children }) {
+function Button({ children, onClick }) {
   return (
-    <button className="px-4 py-2 text-sm bg-orange-400 text-black font-bold rounded-full shadow-md hover:bg-orange-500 transition">
+    <button
+      className="px-4 py-2 text-sm bg-orange-400 text-black font-bold rounded-full shadow-md hover:bg-orange-500 transition"
+      onClick={onClick}
+    >
       {children}
     </button>
   );
